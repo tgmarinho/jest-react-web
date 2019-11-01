6403a114727d8acfb0207109a274ecd91d6a482a
Thiago Marinho <tgmarinho@gmail.com>
2019-11-01 14:46:11 -0400

    Testes Jest React Web - Aula 03 - Testando o formulário

--- src/__tests__/components/Techlist.test.js
+++ src/__tests__/components/Techlist.test.js
@@ -5,10 +5,13 @@ import TechList from "~/components/TechList";
 
 describe("TechList component", () => {
   it("should be able to add new tech", () => {
-    const { getByText, getByTestId, debug } = render(<TechList />);
-    debug();
-    fireEvent.click(getByText("Adicionar"));
-    debug();
+    const { getByText, getByTestId, getByLabelText } = render(<TechList />);
+
+    fireEvent.change(getByLabelText("Tech"), { target: { value: "Node.js " } });
+
+    fireEvent.submit(getByTestId("tech-form"));
+
     expect(getByTestId("tech-list")).toContainElement(getByText("Node.js"));
+    expect(getByLabelText("Tech")).toHaveValue("");
   });
 });
