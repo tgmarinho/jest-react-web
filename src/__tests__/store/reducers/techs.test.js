c27d3de72f2608d53dd707759902a829b3f05090
Thiago Marinho <tgmarinho@gmail.com>
2019-11-04 09:56:40 -0400

    Testes Jest React Web - Aula 07 - Testando reducers

---
+++ src/__tests__/store/reducers/techs.test.js
@@ -0,0 +1,14 @@
+import reducer, {
+  INTIAL_STATE,
+  INITIAL_STATE,
+} from '~/store/modules/techs/reducers';
+
+import * as Techs from '~/store/modules/techs/actions';
+
+describe('Techs reducer', () => {
+  it('ADD_TECH', () => {
+    const state = reducer(INITIAL_STATE, Techs.addTech('Node.js'));
+
+    expect(state).toStrictEqual(['Node.js']);
+  });
+});
