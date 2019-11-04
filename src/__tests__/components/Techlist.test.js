7dd69fa5c73ec11d9d0ca764bd58f5290e987a1d
Thiago Marinho <tgmarinho@gmail.com>
2019-11-04 09:46:16 -0400

    Testes Jest React Web - Aula 06 - Mock do useDispatch

--- src/__tests__/components/Techlist.test.js
+++ src/__tests__/components/Techlist.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { render } from '@testing-library/react';
+import { useSelector, useDispatch } from 'react-redux';
+import { render, fireEvent } from '@testing-library/react';
+import { addTech } from '~/store/modules/techs/actions';
 
 import TechList from '~/components/TechList';
 
@@ -14,11 +15,25 @@ describe('TechList component', () => {
       })
     );
 
-    const { getByTestId, getByText, debug } = render(<TechList />);
-
-    // debug();
+    const { getByTestId, getByText } = render(<TechList />);
 
     expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'));
     expect(getByTestId('tech-list')).toContainElement(getByText('ReactJS'));
+  });
+
+  it('should be able to add new tech', () => {
+    const { getByTestId, getByLabelText } = render(<TechList />);
+
+    const dispatch = jest.fn();
+
+    useDispatch.mockReturnValue(dispatch);
+
+    fireEvent.change(getByLabelText('Tech'), {
+      target: { value: 'Node.js' },
+    });
+    fireEvent.submit(getByTestId('tech-form'));
+
+    expect(dispatch).toHaveBeenCalled();
+    expect(dispatch).toHaveBeenCalledWith(addTech('Node.js'));
   });
 });
--- src/components/TechList/index.js
+++ src/components/TechList/index.js
@@ -1,6 +1,7 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { addTech } from '~/store/modules/techs/actions';
 
 export default function TechList() {
   const [newTech, setNewTech] = useState('');
@@ -8,7 +9,7 @@ export default function TechList() {
   const techs = useSelector(state => state.techs);
 
   function handleAddTech() {
-    dispatch({ type: 'ADD_TECH', payload: { tech: newTech } });
+    dispatch(addTech(newTech));
     setNewTech('');
   }
 
